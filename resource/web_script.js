9a6a2a8089904bd310c10e154c0550e184f3d1ab
aoehiagwina <60975880+aoehiagwina@users.noreply.github.com>
2022-11-15 13:48:29 +0000

    first commit

---
+++ resource/web_script.js
@@ -0,0 +1,15 @@
+//conecting files
+let icon1 = document.getElementById('item 1')
+let icon2 = document.getElementById('item 2')
+let icon3 = document.getElementById('item 3')
+let icon4 = document.getElementById('item 4')
+let icon5 = document.getElementById('item 5')
+
+let text1 = document.getElementById('item A')
+let text2 = document.getElementById('item B')
+let text3 = document.getElementById('item C')
+let text4 = document.getElementById('item D')
+let text5 = document.getElementById('item E')
+
+//Setting interaction for headers navigation
+
